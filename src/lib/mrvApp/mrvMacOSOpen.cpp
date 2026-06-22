91f8383c39720ac4b5c2e4694d01646c946cb5b6
tinker <tinker@tinkers-MacBook-Pro.local>
2026-06-22 09:52:40 +0800

    Handle macOS open-file events before FLTK callback setup

--- src/lib/mrvApp/mrvMacOSOpen.cpp
+++ src/lib/mrvApp/mrvMacOSOpen.cpp
@@ -6,10 +6,12 @@
 
 #import <AppKit/AppKit.h>
 #import <Foundation/Foundation.h>
+#import <objc/runtime.h>
 
 namespace
 {
     mrv::MacOSOpenCallback gOpenCallback = nullptr;
+    Class gSwizzledDelegateClass = Nil;
 
     void openPath(NSString* path)
     {
@@ -17,6 +19,61 @@ namespace
             return;
 
         gOpenCallback([path fileSystemRepresentation]);
+    }
+
+    BOOL mrvApplicationOpenFile(
+        id self, SEL _cmd, NSApplication* application, NSString* filename)
+    {
+        (void)self;
+        (void)_cmd;
+        (void)application;
+
+        openPath(filename);
+        return YES;
+    }
+
+    void mrvApplicationOpenFiles(
+        id self, SEL _cmd, NSApplication* application, NSArray* filenames)
+    {
+        (void)self;
+        (void)_cmd;
+
+        for (NSString* filename in filenames)
+            openPath(filename);
+
+        [application replyToOpenOrPrint:NSApplicationDelegateReplySuccess];
+    }
+
+    void installDelegateOpenHandlers()
+    {
+        id<NSApplicationDelegate> delegate = [NSApp delegate];
+        if (!delegate)
+            return;
+
+        Class delegateClass = object_getClass(delegate);
+        if (!delegateClass || delegateClass == gSwizzledDelegateClass)
+            return;
+
+        SEL openFileSelector = @selector(application:openFile:);
+        Method openFileMethod =
+            class_getInstanceMethod(delegateClass, openFileSelector);
+        if (openFileMethod)
+        {
+            method_setImplementation(
+                openFileMethod, (IMP)mrvApplicationOpenFile);
+        }
+        else
+        {
+            class_addMethod(
+                delegateClass, openFileSelector,
+                (IMP)mrvApplicationOpenFile, "B@:@@");
+        }
+
+        class_addMethod(
+            delegateClass, @selector(application:openFiles:),
+            (IMP)mrvApplicationOpenFiles, "v@:@@");
+
+        gSwizzledDelegateClass = delegateClass;
     }
 }
 
@@ -70,6 +127,8 @@ namespace mrv
     void installMacOSOpenDocumentHandler(MacOSOpenCallback callback)
     {
         gOpenCallback = callback;
+
+        installDelegateOpenHandlers();
 
         static MRVMacOSOpenHandler* handler = nil;
         if (!handler)
